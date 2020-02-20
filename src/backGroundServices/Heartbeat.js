9804921414e28190cb74482f7d9d04decdcaafaa
admin <admin@huelite.in>
2020-02-21 05:21:20 +0530

    Heartbeat Debugged by Abhimanyu
    * for loop was not working for all the devices, updated with map function
    
    * await removed from function as it was delaying the further calls to connect_to_device function in consicutive calls

--- src/backGroundServices/Heartbeat.js
+++ src/backGroundServices/Heartbeat.js
@@ -1,46 +1,56 @@
-import { connectToDevice } from './Connector';
-import { getCurrentTimeStamp, addTimeIntoTimeStamp, findTimestampDiffInSec } from '../util/DateTimeUtil';
+import {connectToDevice} from './Connector';
+import {
+  getCurrentTimeStamp,
+  addTimeIntoTimeStamp,
+  findTimestampDiffInSec,
+} from '../util/DateTimeUtil';
 import _ from 'underscore';
 
 const heartBeatHandler = async (deviceList, updateList) => {
-    console.log("HeatBeat Device list: ",deviceList);
-    // if(deviceList.lenght>0){
-        for (let i = 0; i < deviceList.length; i++) {
-            if (deviceList[i].Web_Socket && Object.entries(deviceList[i].Web_Socket)) {
-                let ts = getCurrentTimeStamp();
-                let diff = findTimestampDiffInSec(deviceList[i].Last_WS_Msg_Received_Time_Stamp, ts);
-                console.log("DIFFERENCE , ",diff);
-                if(diff > 8){
-                    deviceList[i].Web_Socket = null
-                    deviceList[i].Last_WS_Msg_Received_Time_Stamp = 0;
-                    deviceList[i].Connected = false;
-                    if (i === deviceList.length - 1) {
-                        updateList(deviceList); 
-                    }
-                }
-                else if (diff >= 3 && deviceList[i].Web_Socket && deviceList[i].Web_Socket.send) {
-                    deviceList[i].Web_Socket.send("Heartbeat");
-                }
-                
-            }
-            else {
-                console.log("IDHAR AAYA");
-                await connectToDevice(deviceList[i].IP_Address, wsRes => { console.log("FROM HEARTBEAT")},
-                    (mgsRecievedRes, wsRes) => {
-                        if (mgsRecievedRes && wsRes) {
-                            deviceList[i].Web_Socket = wsRes
-                            deviceList[i].Last_WS_Msg_Received_Time_Stamp = getCurrentTimeStamp();
-                            deviceList[i].Connected = true;
-                        }
-                        if (i === deviceList.length - 1) {
-                            updateList(deviceList);
-                        }
-                    });
-            }
+  console.log('HeatBeat Device list: ', deviceList);
+  // if(deviceList.lenght>0){
+  deviceList.map((item, i) => {
+    console.log('test :: ' + i);
+    console.log('Mac :: ' + item.Mac);
+    if (item.Web_Socket && Object.entries(item.Web_Socket)) {
+      let ts = getCurrentTimeStamp();
+      let diff = findTimestampDiffInSec(
+        item.Last_WS_Msg_Received_Time_Stamp,
+        ts,
+      );
+      console.log('DIFFERENCE , ', diff);
+      if (diff > 8) {
+        item.Web_Socket = null;
+        item.Last_WS_Msg_Received_Time_Stamp = 0;
+        item.Connected = false;
+        if (i === deviceList.length - 1) {
+          updateList(deviceList);
         }
-    // }
-}
+      } else if (diff >= 3 && item.Web_Socket && item.Web_Socket.send) {
+        item.Web_Socket.send('Heartbeat');
+      }
+    } else {
+      console.log('IDHAR AAYA');
+      connectToDevice(
+        item.IP_Address,
+        wsRes => {
+          console.log('FROM HEARTBEAT');
+        },
+        (mgsRecievedRes, wsRes) => {
+          if (mgsRecievedRes && wsRes) {
+            item.Web_Socket = wsRes;
+            item.Last_WS_Msg_Received_Time_Stamp = getCurrentTimeStamp();
+            item.Connected = true;
+          }
+          if (i === deviceList.length - 1) {
+            updateList(deviceList);
+          }
+        },
+      );
+    }
+    if (item.Connected) console.log('Connected');
+    else console.log('Not Connected');
+  });
+};
 
-export {
-    heartBeatHandler
-}
+export {heartBeatHandler};

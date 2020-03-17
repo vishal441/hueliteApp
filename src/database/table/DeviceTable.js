dde266c84a8627435c6619dc4ac54bfa95c47f7b
admin <admin@huelite.in>
2020-03-17 16:23:24 +0530

    ---------

--- src/database/table/DeviceTable.js
+++ src/database/table/DeviceTable.js
@@ -1,20 +1,19 @@
+import { DeviceSchema } from "../Schema"
+import { insertOrUpdateQuery, getQuery, deleteSchema, deleteObject } from "../DbAction"
 
-import {DeviceSchema} from '../Schema';
-import {insertOrUpdateQuery, getQuery, deleteSchema} from '../DbAction';
-
-export const insertDevices = (deviceArr) => {
-    insertOrUpdateQuery(DeviceSchema.name,deviceArr, cb => {
-   });
-  
+export const insertDevices = deviceArr => {
+    insertOrUpdateQuery(DeviceSchema.name, deviceArr, cb => {})
 }
 
-export const getDeviceListFromDb = async(callback) => {
-    let deviceRes = await getQuery(DeviceSchema.name,false);
-    return deviceRes; 
+export const getDeviceListFromDb = async callback => {
+    let deviceRes = await getQuery(DeviceSchema.name, false)
+    return deviceRes
 }
 
 export const deleteDeviceTable = () => {
-    deleteSchema([DeviceSchema.name], cb => {
+    deleteSchema([DeviceSchema.name], cb => {})
+}
 
-    });
+export const deleteDevice = (filter, callback) => {
+    deleteObject(DeviceSchema.name, filter, callback)
 }
--- src/database/table/UserInfoTable.js
+++ src/database/table/UserInfoTable.js
@@ -1,27 +1,19 @@
+import { UserInfoSchema } from "../Schema"
+import { insertOrUpdateQuery, getQuery, deleteSchema } from "../DbAction"
 
-import {UserInfoSchema} from '../Schema';
-import {insertOrUpdateQuery, getQuery, deleteSchema} from '../DbAction';
-
-export const insertUserInfo = (userInfo) => { 
-    insertOrUpdateQuery(UserInfoSchema.name,userInfo, cb => {
-       
-   });
-  
+export const insertUserInfo = userInfo => {
+    insertOrUpdateQuery(UserInfoSchema.name, userInfo, cb => {})
 }
 
-export const getUserInfoFromDb = async() => {
-    let dbRes = await getQuery(UserInfoSchema.name,false);
-    if(dbRes.success){
-       return dbRes.data[0];
-    }
-    else{
-        return false;
+export const getUserInfoFromDb = async () => {
+    let dbRes = await getQuery(UserInfoSchema.name, false)
+    if (dbRes.success) {
+        return dbRes.data[0]
+    } else {
+        return false
     }
 }
 
 export const deleteUserInfoTable = () => {
-    deleteSchema([UserInfoSchema.name], cb => {
-        
-    });
+    deleteSchema([UserInfoSchema.name], cb => {})
 }
-
